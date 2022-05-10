0e418ebb27124f7d8f241f4ca6e23dc55d2becdb
Md Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2022-05-10 11:27:19 +0600

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,8 @@
+// Powershell command in winget 
+winget install --id=Notepad++.Notepad++ -e  ; 
+winget install --id=WhatsApp.WhatsApp -e  ; 
+winget install --id=Microsoft.dotNetFramework -e  ; 
+winget install --id=7zip.7zip -e  ; 
+winget install --id=OmicronLab.Avro -e  ; 
+winget install --id=Git.Git -e  ; 
+winget install --id=Microsoft.GitCredentialManagerCore -e 
